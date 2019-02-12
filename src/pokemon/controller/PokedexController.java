ed2f0dd7becd73b9ade8c89556244860ba602cc1
Guillermo <memo33101@gmail.com>
2019-02-12 08:59:19 -0700

    added text and update methods for controller

--- src/pokemon/controller/PokedexController.java
+++ src/pokemon/controller/PokedexController.java
@@ -19,4 +19,28 @@ public class PokedexController
 	{
 		
 	}
+	
+	public void updatePokemon(int index, String[] data)
+	{
+		if(data.length == 5)
+		{
+			Pokemon current = pokemonList.get(index);
+			current.setAttackPoints(Integer.parseInt(data[0]));
+			current.setEnhancementModifier(Double.parseDouble(data[1]));
+			current.setHealthPoints(Integer.parseInt(data[2]));
+			current.setName(data[3]);
+			current.setCanEvolve(Boolean.parseBoolean(data[4]));
+		}
+	}
+	
+	public String[] buildPokedexText()
+	{
+		String [] names = new String[pokemonList.size()];
+		
+		for(int i = 0; i < pokemonList.size(); i++)
+		{
+			names[i] = pokemonList.get(i).getName();
+		}
+		return names;
+	}
 }
